3f57d761e6ae6bf261baec6080b39542dc18ade4
v.khungureev <v.khungureev@souzmult.ru>
2024-09-26 18:09:13 +0300

    Added python-slim Set ubuntu version Calculating the checksum

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-source build_tag.sh ubuntu 0.1.0 3555bcfd670e134e8360ad934cb5bad1bbe2a7dad24ba7cafa0a3bb8b23c6444
+DEFAULT_TAG=0.1.0
+
+tag=${1:-$DEFAULT_TAG}
+
+function build_tag {
+    source build_tag.sh $1 $tag $2
+}
+
+build_tag ubuntu
+build_tag python-slim
--- build_tag.sh
+++ build_tag.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build . -f docker_files/$1 -t static-jinja-plus:$2 --rm --build-arg TAG=$2 --build-arg HASH=$3
+docker_tag=${3:-2}
+url=https://github.com/MrDave/StaticJinjaPlus/archive/refs/tags/$2.tar.gz
+hash=$(curl -sL $url | sha256sum | awk '{ print $1 }')
+
+docker build . -f docker_files/$1 -t static-jinja-plus:$docker_tag --rm --build-arg TAG=$2 --build-arg HASH=$hash
